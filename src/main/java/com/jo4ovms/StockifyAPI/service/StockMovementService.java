c54228c136e8fbc361a6636a2b1d8d0eb77b4a10
jo4ovms <joaovictormacields@gmail.com>
2024-09-05 16:19:26 -0300

    feat(service): add StockMovementService with logic for stock movements
    - Implemented StockMovementService to handle stock movement operations.
    - Added validations for stock availability during outbound movements.
    - Integrated with StockRepository and StockMovementRepository.

---
+++ src/main/java/com/jo4ovms/StockifyAPI/service/StockMovementService.java
@@ -0,0 +1,45 @@
+package com.jo4ovms.StockifyAPI.service;
+
+import com.jo4ovms.StockifyAPI.exception.InsufficientStockException;
+import com.jo4ovms.StockifyAPI.exception.ResourceNotFoundException;
+import com.jo4ovms.StockifyAPI.exception.ValidationException;
+import com.jo4ovms.StockifyAPI.mapper.StockMovementMapper;
+import com.jo4ovms.StockifyAPI.model.DTO.StockMovementDTO;
+import com.jo4ovms.StockifyAPI.model.Stock;
+import com.jo4ovms.StockifyAPI.model.StockMovement;
+import com.jo4ovms.StockifyAPI.repository.StockMovementRepository;
+import com.jo4ovms.StockifyAPI.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockMovementService {
+    @Autowired
+    private StockMovementRepository stockMovementRepository;
+
+    @Autowired
+    private StockMovementMapper stockMovementMapper;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    public StockMovementDTO registerMovement(StockMovementDTO stockMovementDTO) {
+        Stock stock = stockRepository.findById(stockMovementDTO.getStockId())
+                .orElseThrow(() -> new ResourceNotFoundException("Stock with id " + stockMovementDTO.getStockId() + " not found."));
+
+        if (stockMovementDTO.getMovementType() == StockMovement.MovementType.OUTBOUND && stock.getQuantity() < stockMovementDTO.getQuantityChange()) {
+            throw new InsufficientStockException("Not enough stock available for this movement.");
+        }
+        if (stockMovementDTO.getQuantityChange() <= 0) {
+            throw new ValidationException("Quantity must be greater than zero");
+        }
+
+        stock.setQuantity(stock.getQuantity() - stockMovementDTO.getQuantityChange());
+        stockRepository.save(stock);
+
+        StockMovement stockMovement = stockMovementMapper.toStockMovement(stockMovementDTO);
+        stockMovementRepository.save(stockMovement);
+
+        return stockMovementMapper.toStockMovementDTO(stockMovement);
+    }
+}
