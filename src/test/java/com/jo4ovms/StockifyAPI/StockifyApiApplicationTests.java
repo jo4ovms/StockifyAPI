84096e9e2b3917a88be468e60b737b4840b0b8cc
Joao Victor <joaovictormacields@gmail.com>
2024-09-05 13:59:28 -0300

    Initial commit

---
+++ src/test/java/com/jo4ovms/StockifyAPI/StockifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo4ovms.StockifyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
